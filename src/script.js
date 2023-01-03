56c8bd0ec5b3d8841b433ca6e01e855150f11c94
Marks Henrik <markshenrik13@gmail.com>
2023-01-02 21:35:57 -0300

    Modificações feitas no CSS e no JS

--- src/script.js
+++ src/script.js
@@ -6,20 +6,24 @@ enviar.addEventListener("click", (e) => {
   e.preventDefault();
 
   campos.forEach((campo, index) => {
-    if(campo.value == ""){
+    if (campo.value == "") {
       campo.classList.add("erro");
-      avisoArr[index].classList.add("alerta-erro")
+      avisoArr[index].classList.add("alerta-erro");
     }
-  })
+  });
 });
 
-function bordaVerde(input) {
-  input.classList.add("borda-verde");
-  let bordaVermelha = input.classList.contains("erro");
-  let inputIndex = parseFloat(input.classList[1]);
-  if (bordaVermelha) {
-    input.classList.remove("erro");
 
-    avisoArr[inputIndex].classList.remove("alerta-erro");
-  }
-}
+
+campos.forEach((campo) => {
+  campo.addEventListener("input", () => {
+    campo.classList.add("borda-verde");
+    let bordaVermelha = campo.classList.contains("erro");
+    let inputIndex = parseFloat(campo.classList[1]);
+    if (bordaVermelha) {
+      campo.classList.remove("erro");
+
+      avisoArr[inputIndex].classList.remove("alerta-erro");
+    }
+  });
+});
